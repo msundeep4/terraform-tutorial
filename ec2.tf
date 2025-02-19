efd4a24e8bf8b70471adca01afa3948723604363
msundeep4 <60873838+msundeep4@users.noreply.github.com>
2025-02-19 18:54:26 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+# Origiona code EC2 instance resource
+//resource "aws_instance" "web" {
+//  ami           = data.aws_ami.ubuntu.id
+//  instance_type = "t3.micro"
+//
+//  tags = {
+//    Name = "HelloWorld"
+//  }
+//}
+
+# Defining/Providing an EC2 instance resource. This allows instances to be created, updated, and deleted. Instances also support provisioning.
+resource "aws_instance" "web" {
+  ami           = "ami-09245d5773578a1d6"
+  instance_type = "t3.micro"
+  count = 5
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
